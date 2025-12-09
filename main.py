0fa5a43bef506a36d81142982ecefb542cb1e092
b_bhanu <bhanu.bhanu1@dellteam.com>
2025-12-09 04:03:42 -0600

    updated

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 n = "(1+3+(1+2)+4+(2+5))+6"
-
 value = 0 
-
 loopval = 0
-
 
 def damn(i):
     global loopval , value
-    print("value" , value)
-
+    #print("value" , value)
     loopval = 0
     for j in range(i+1 , len(n)):
         try:
             loopval = int(n[j]) + loopval
         except:
             if n[j] == "(":
-                print("loopval" , loopval)
+                #print("loopval" , loopval)
                 value = loopval + value
                 damn(j)
             if n[j] == ')':
                 value = loopval + value
                 return
-
-
+               
 for i in range(len(n)):
     if n[i] =='(':
         damn(i)
 
-
+    
 print(value)
